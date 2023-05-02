67b0aed2f90e1c17b26075c683d67b8464a5ccb2
PADURU VIGNA TEJ REDDY <86559881+vignatej@users.noreply.github.com>
2023-05-02 09:27:33 +0530

    Update settings.py

--- internalAppBackend/settings.py
+++ internalAppBackend/settings.py
@@ -125,23 +125,23 @@ WSGI_APPLICATION = 'internalAppBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'internalAppBackend',
-        'USER': 'postgres',
-        'PASSWORD': 'asd123zxc@',
-        'HOST':"localhost",
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'internalAppBackend',
+#         'USER': 'postgres',
+#         'PASSWORD': 'asd123zxc@',
+#         'HOST':"localhost",
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
